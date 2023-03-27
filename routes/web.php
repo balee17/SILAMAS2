4c5dea9b1a181eaf3503489c00ff3ffdfa22b545
balee17 <iqbalmaulana8533@gmail.com>
2023-03-27 08:31:18 +0700

    no message

--- routes/web.php
+++ routes/web.php
@@ -64,3 +64,68 @@ Route::get('/', [IndexController::class, 'index'])->middleware('guest');
 // Route::get('admin', function () { return view('auth.admin.dashboard'); })->middleware('CheckRoleUsers:admin');
 // Route::get('petugas', function () { return view('auth.petugas.petugas'); })->middleware(['CheckRoleUsers:petugas,admin']);
 // Route::get('/', function () { return view('index'); })->middleware(['CheckRoleUsers:user']);
+Route::get('/login', [LoginController::class, 'create'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'store'])->name('login')->middleware('guest');
+
+
+// Admin/Petugas
+Route::prefix('admin')
+    ->middleware(['auth', 'admin'])
+    ->group(function() {
+        Route::get('/', 'DashboardController@index')->name('dashboard');
+        Route::post('logout', 'DashboardController@logout')->name('logout');
+
+        //User Detail
+        Route::get('user', 'DashboardAdminController@index')->name('index');
+        Route::post('user', 'DashboardAdminController@search')->name('search.user');
+        Route::get('user/{id}','DashboardAdminController@show')->name('detail.user');
+
+        // Laporan Detail
+        Route::get('laporan', 'DashboardAdminController@laporan')->name('laporan');
+        Route::get('laporan/{id}','DashboardAdminController@detail_laporan')->name('detail.laporan');
+        Route::post('laporan','DashboardAdminController@store')->name('detail.store');
+
+        //delete
+        Route::post('laporan/{id}','DashboardAdminController@destroy')->name('destroy.laporan');
+        Route::post('user/{id}','DashboardAdminController@delete')->name('delete.user');
+
+        // Search laporan
+        Route::get('search', 'SearchAdminController@index')->name('index');
+        Route::post('search', 'SearchAdminController@search')->name('search.laporan');
+
+        //Tanggapan Laporan User
+        Route::get('tanggapan', 'DashboardAdminController@tanggapan')->name('tanggapan.user');
+});
+
+
+// Masyarakat
+Route::prefix('user')
+    ->middleware(['auth', 'MasyarakatMiddleware'])
+    ->group(function() {
+                //Home
+				Route::get('/', 'HomeController@index')->name('user-dashboard');
+                //Create Laporan
+                Route::resource('laporans', 'ReportingController')->middleware('auth');
+                //Search Home
+                Route::get('search', [SearchController::class, 'search'])->name('search');
+                //Dasboard User Laporan
+                Route::get('dashboard', [DashboardUserController::class, 'index'])->middleware('auth')->name('dashboard');
+                //Profle user
+                Route::get('profile', [DashboardUserController::class, 'profile'])->middleware('auth')->name('profile');
+                Route::post('profile', [DashboardUserController::class, 'update'])->middleware('auth')->name('profile');
+                //Detail Laporan
+                Route::get('detail/{id}','DashboardUserController@show')->name('detail')->middleware('check.report.access');
+                Route::get('error','DashboardUserController@error')->name('error');
+                //LeaderBoard
+                Route::get('leaderboard','DashboardUserController@leaderboard')->name('leaderboard');
+                //Feedback jika complete
+                Route::post('feedback','DashboardUserController@store')->name('feedback.complete');
+
+
+});
+
+
+
+
+
+require __DIR__.'/auth.php';
